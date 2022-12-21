8bdd31397ffffeaecd1aacc2259d7b9725eb320e
Ishayadav25 <115414374+Ishayadav25@users.noreply.github.com>
2022-12-21 09:05:59 +0530

    Add files via upload

---
+++ question7.py
@@ -0,0 +1,5 @@
+first_set = {27,43,34}
+second_set = {34,93,22,27,43,53,48}
+print("First set is subset of second set -", first_set.issubset(second_set))
+print("Second set is subset of First set - ", second_set.issubset(first_set))
+

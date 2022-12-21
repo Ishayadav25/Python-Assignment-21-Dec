c6ddb4ea4e59098411376e44e29604d0f0f6b680
Ishayadav25 <115414374+Ishayadav25@users.noreply.github.com>
2022-12-21 08:32:27 +0530

    Add files via upload

---
+++ queston2.py
@@ -0,0 +1,9 @@
+main_list = [54, 44, 27,79,91,41]
+element = main_list.pop(4)
+print("List After removing element at index 4 ", main_list)
+
+main_list.insert(2, element)
+print("List after Adding element at index 2 ", main_list)
+
+main_list.append(element)
+print("List after Adding element at last ", main_list)
